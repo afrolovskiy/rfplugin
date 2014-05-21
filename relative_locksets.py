81f6e3f209ca47d1a69946db8d9c999374a62866
alex <alex@localhost.localdomain>
2014-05-21 22:29:03 +0400

    fixed buf of core building

--- relative_locksets.py
+++ relative_locksets.py
@@ -5,7 +5,14 @@ class RelativeLocksetAnalyzer(gcc.GimplePass):
     K = 2
 
     def execute(self, fun):
+        print '================================'
+        print fun.decl.name
+        for bb in fun.cfg.basic_blocks:
+            for edge in bb.succs:
+                print '({},{})'.format(bb.index, edge.dest.index)
         core = self.build_code(fun)
+        print 'core: ', core
+        #import ipdb; ipdb.set_trace()
 
     @staticmethod
     def build_code(fun):
@@ -22,12 +29,17 @@ class RelativeLocksetAnalyzer(gcc.GimplePass):
             if block.index == fun.cfg.exit.index:
                 return set(path)
 
-            cores, count = [], 0
+            cores = []
             for edge in block.succs:
                 next_block = edge.dest
                 if count_block(next_block, path) <= RelativeLocksetAnalyzer.K:
                     cores.append(walk(next_block, list(path)))
-                    count = count + 1
+
+            cores = [c for c in cores if c is not None]
+            count = len(cores)
+
+            if count == 0:
+                return None
 
             core = cores[0]
             for idx in range(1, count):

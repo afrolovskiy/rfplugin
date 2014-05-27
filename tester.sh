b780567b7ca39676be4715070e041f1020a28350
alex <alex@localhost.localdomain>
2014-05-28 00:54:29 +0400

    tmp

--- tester.sh
+++ tester.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 REPEAT=5
 #FILES='test1.c test2.c test3.c test4.c test5.c test6.c test7.c test8.c test9.c test10.c'
-FILES='test9.c'
+FILES='test11_2.c test11_4.c test11_6.c test11_8.c test11_10.c test11_12.c'
 
 for fname in $FILES
 do
 	echo 'file: '$fname
 
-	for with_main in 'false' 'true'
+	for with_main in 'false'
 	do
 		echo 'with main: '$with_main
                 export WITH_MAIN=$with_main
 
-		for max_level in 1 2 3 4 5 6 7 8 9
+		for max_level in 1 2 3
 		do
 			echo 'max level: '$max_level
 			export MAX_LEVEL=$max_level
@@ -23,7 +23,7 @@ do
 			for ((i = 0; i < $REPEAT; i++))
 			do
 				echo 'iteration: '$i
-				./gcc-pyplugin plugin.py tests/$fname -lpthread >> $out
+				./gcc-pyplugin plugin.py tests/$fname -lpthread  >> $out
 			done
 		done
 	done
--- tests/test11.c
+++ tests/test11.c
@@ -53,13 +53,13 @@ int main(int argc, char** argv) {
 
     // create readers
     pthread_create(&thread1, NULL, reader, NULL);
-    pthread_create(&thread2, NULL, reader, NULL);
-    pthread_create(&thread3, NULL, reader, NULL);
-    pthread_create(&thread4, NULL, reader, NULL);
+    pthread_create(&thread1, NULL, reader, NULL);
+    pthread_create(&thread1, NULL, reader, NULL);
 
     // create writers
     pthread_create(&thread5, NULL, writer, NULL);
-    pthread_create(&thread6, NULL, writer, NULL);
+    pthread_create(&thread5, NULL, writer, NULL);
+    pthread_create(&thread5, NULL, writer, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
